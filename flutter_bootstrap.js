e12fba4cfe633816419b9587ca679e2bd999f377
Miguel Cota <mikeest1972@gmail.com>
2025-05-15 17:41:23 -0500

    v2.2
    New UI and log out functinality

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1305918493"
+    serviceWorkerVersion: "304506719"
   }
 });

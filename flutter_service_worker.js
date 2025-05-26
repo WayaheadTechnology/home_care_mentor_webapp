a940fe9b21100a97aa0d3d3a71b106d79a5201f6
Miguel Cota <mikeest1972@gmail.com>
2025-05-26 15:11:44 -0500

    v2.3.2
    - Added trainings to provider drill down

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b5338cdbe47a2b825dea9efb9f0ecf9d",
+const RESOURCES = {"flutter_bootstrap.js": "99db00d07de858bbe6f6952f1324f6c9",
 "version.json": "ed722ba5a7c9ee7903340cbd8c165821",
 "index.html": "53139d6a1a1eee694fc737278fc2c8f3",
 "/": "53139d6a1a1eee694fc737278fc2c8f3",
-"main.dart.js": "078d630833361c585625bd237e9e6698",
+"main.dart.js": "15b218864d27267285f7ee9276d45c8d",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",

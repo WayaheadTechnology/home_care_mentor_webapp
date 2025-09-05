e14dd04877f83b617ecf5370deb126ee426a30e5
nimishporwal47b <123079657+nimishporwal47b@users.noreply.github.com>
2025-09-05 19:56:09 +0530

    feat: order details payment status and retry integration.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9337b99e47c366ca816c2b2ff599444a",
+const RESOURCES = {"flutter_bootstrap.js": "0265f24400d2b28a5e343149e908f2cc",
 "version.json": "ed722ba5a7c9ee7903340cbd8c165821",
 "index.html": "e20249225ae9920a74595be109ee6b81",
 "/": "e20249225ae9920a74595be109ee6b81",
-"main.dart.js": "aa99f1c914ff22fdfb0f285ebc4327d8",
+"main.dart.js": "9f0190e7963063d0a6bb45f0edc83218",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "955e9582de8c00d394c6e6b538498c6a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -31,7 +31,7 @@ const RESOURCES = {"flutter_bootstrap.js": "9337b99e47c366ca816c2b2ff599444a",
 "assets/lib/assets/empty_profile_picture.png": "3961eab7a198e63aebec44ef26bf195f",
 "assets/lib/assets/calendar.svg": "bb705e00ee50919929fe913d0f81b269",
 "assets/AssetManifest.bin": "874f366e9a8387abe04284dc9497dd18",
-"assets/fonts/MaterialIcons-Regular.otf": "14e49deefe9fb36ba8c709afd79a4f37",
+"assets/fonts/MaterialIcons-Regular.otf": "84a98bcdb7f587ec3288415fe5e3b623",
 "canvaskit/skwasm.js": "ea559890a088fe28b4ddf70e17e60052",
 "canvaskit/skwasm.js.symbols": "e72c79950c8a8483d826a7f0560573a1",
 "canvaskit/canvaskit.js.symbols": "bdcd3835edf8586b6d6edfce8749fb77",
